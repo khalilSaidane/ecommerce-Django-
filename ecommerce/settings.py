ad19b10a985c1c584d79c83c0aea8a27b5454443
khalilsaidane <khalilsaidane96@hotmail.com>
2020-05-17 15:09:52 +0100

    logout redirect url

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 LOGIN_REDIRECT_URL = '/products/list'
