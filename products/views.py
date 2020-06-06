ab0dccf609116885df1623718f44941b6563051f
khalilsaidane <khalilsaidane96@hotmail.com>
2020-06-06 17:58:10 +0100

    using agreggation in review avg

--- products/views.py
+++ products/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, redirect
 
 from .forms import CreateProductForm
 from .models import Product
+from django.db.models import Avg
+
 
 
 @login_required(login_url='/accounts/login')
@@ -27,8 +29,8 @@ def product_detail_view(request, id, *args, **kwargs):
     product_review_count = product.review_set.count()
     rate = 0
     if reviews.count() > 0:
-        rate = statistics.mean([review.rate for review in reviews])
-        rate = float("{:.2f}".format(rate))
+        # rate = statistics.mean([review.rate for review in reviews])
+        rate = product.review_set.aggregate(Avg('rate'))['rate__avg']
     context = {
         'product': product,
         'reviews': reviews[:2],
